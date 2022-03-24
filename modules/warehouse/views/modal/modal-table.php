2ef2f423996fae727d7f4cb0754537c719787e6b
crmarm <crmarm313@gmail.com>
2022-03-24 13:03:58 +0400

    modal style

--- modules/warehouse/views/modal/modal-table.php
+++ modules/warehouse/views/modal/modal-table.php
@@ -1,46 +1,93 @@
-<div class="modal-dialog" role="document">
+<div class="w-50 m-auto position-relative" role="document" style="top: 50px;">
     <div class="modal-content">
         <form method="post" action="/warehouse/warehouse/change-table-rows">
             <input type="hidden" name="_csrf" value="<?php echo Yii::$app->request->csrfToken; ?>">
             <input type="hidden" name="page" value="<?php echo $page; ?>">
             <div class="modal-header">
-                <h5 class="modal-title">Modal title</h5>
+                <h5 class="modal-title"><i class="fa fa-wrench mr-3"></i>Modal title</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <div class="modal-body" style="display:flex;">
-                <div class="d-flex flex-column sortable connectedSortable" style="min-width: 300px">
+            <div class="d-flex justify-content-between" style="width: 98%;margin-left:1%">
+                <div class="d-flex flex-column " style="width: 22%;">
+                    <label>asas</label>
+                    <div class="input-group">
+                        <input type="number" class="form-control" placeholder="Input group example" aria-label="Input group example" aria-describedby="btnGroupAddon">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text" id="btnGroupAddon">count pages</div>
+                        </div>
+                    </div>
+                    <p>asdasdasd</p>
+                </div>
+                <div class="d-flex flex-column" style="width: 22%;">
+                    <label>asas</label>
+                    <input type="text" class="form-control">
+                    <p>asdasdasd</p>
+                </div>
+                <div class="d-flex flex-column" style="width: 22%;">
+                    <label>asas</label>
+                    <input type="text" class="form-control">
+                    <p>asdasdasd</p>
+                </div>
+                <div class="d-flex flex-column" style="width: 22%;">
+                    <label>asas</label>
+                    <input type="text" class="form-control">
+                    <p>asdasdasd</p>
+                </div>
+            </div>
+            <label class=" text-center border-bottom border-top mt-3 p-2" style="width: 98%;margin-left:1%">kargavorumner</label>
+            <div class="modal-body d-flex justify-content-between" >
+                <div class="d-flex flex-column sortable connectedSortable modal-table" >
+                    <label class="modal-label">Tesaneli syuner</label>
                     <?php if(isset($columnsActive)):
                         foreach ($columnsActive as $c): ?>
-                            <span class="ui-state-default"><?php echo $c->row_name; ?>
+                            <span><i class="fa fa-eye mr-2"></i><?php echo $c->row_name; ?>
                                 <input type="hidden" name="row[id][]" value="<?php echo $c->id; ?>">
                             </span>
                         <?php endforeach;
                     endif; ?>
                 </div>
                 <input type="hidden" name="row[id][]" value="passive-data">
-                <div class="d-flex flex-column sortable connectedSortable" style="min-width: 300px">
+                <div style="width: 5%;text-align: center"><i class='fas fa-arrows-alt-h' style="font-size: 35px"></i></div>
+                <div class="d-flex flex-column sortable connectedSortable modal-table" >
+                    <label class="modal-label">Antesaneli syuner</label>
                     <?php if(isset($columnsPassive)):
                         foreach ($columnsPassive as $c): ?>
-                            <span class="draggable ui-state-highlight" ><?php echo $c->row_name; ?>
+                            <span class="draggable " ><i class="fa fa-eye-slash mr-2"></i><?php echo $c->row_name; ?>
                                 <input type="hidden" name="row[id][]" value="<?php echo $c->id; ?>">
                             </span>
                         <?php endforeach;
                     endif; ?>
                 </div>
             </div>
-            <div class="modal-footer">
-                <input type="submit" value="<?php echo Yii::t('app', 'Save');?>">
+            <div class="modal-footer ">
+                <input class="btn btn-primary" type="submit" value="<?php echo Yii::t('app', 'Save');?>">
             </div>
         </form>
     </div>
 </div>
 
 <style>
-    .ui-state-default {
-        font-size: 20px;
-        padding: 12px;
+    .modal-table {
+        min-width: 300px;
+        width: 40%;
+        border: 1px solid #bcadad;
+        padding: 10px;
+    }
+    .modal-table span {
+        border:1px solid #bcadad;
+        margin-bottom: 5px;
+        padding: 5px 10px;
+    }
+    .modal-label {
+        border:1px solid #bcadad;
+        margin-bottom: 5px;
+        padding-left: 10px;
+        color: #fff;
+        background: #1b55e2;
+        font-size: 18px;
+        line-height: 40px;
     }
 </style>
 <script>
