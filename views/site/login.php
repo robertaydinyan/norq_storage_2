8ed3ea7d167e02d2ab89701c43784a09311b2442
crmarm <crmarm313@gmail.com>
2022-04-04 18:18:22 +0400

    login style

--- views/site/login.php
+++ views/site/login.php
@@ -10,6 +10,14 @@ use yii\bootstrap\ActiveForm;
 $this->title = Yii::t('app', 'Вход в систему');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+<style>
+    .login-input {
+        height: 37px!important;
+    }
+    .login-btn,.login-btn:hover,.login-btn:after,.login-btn:focus {
+        background: #0055a5!important;
+    }
+</style>
 <div class="site-login h-100">
     <div class="container h-100">
         <div class="row justify-content-md-center align-items-center h-100">
@@ -26,16 +34,16 @@ $this->params['breadcrumbs'][] = $this->title;
                             ],
                         ]); ?>
 
-                        <?= $form->field($model, 'username')->textInput(['autofocus' => true])->label(Yii::t('app', 'Имя пользователя')) ?>
+                        <?= $form->field($model, 'username')->textInput(['autofocus' => true,'class' => 'login-input form-control'])->label(Yii::t('app', 'Имя пользователя')) ?>
 
-                        <?= $form->field($model, 'password')->passwordInput()->label(Yii::t('app', 'Пароль')) ?>
+                        <?= $form->field($model, 'password')->passwordInput(['autofocus' => true,'class' => 'login-input form-control'])->label(Yii::t('app', 'Пароль')) ?>
 
                         <?= $form->field($model, 'rememberMe')->checkbox([
                             'template' => "{input} {label} {error}",
                         ])->label(Yii::t('app', 'Запомни меня')) ?>
 
                         <div class="form-group">
-                            <?= Html::submitButton('Login', ['class' => ' btn-block c-btn', 'name' => 'login-button']) ?>
+                            <?= Html::submitButton('Login', ['class' => ' btn-block c-btn login-btn', 'name' => 'login-button']) ?>
                         </div>
 
                         <?php ActiveForm::end(); ?>
